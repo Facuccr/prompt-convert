e34c85268d6cd9ec8f85c99aa29a35d760ebbcb6
Facundo Cristaldo <facundocristlado146@gmail.com>
2025-05-10 20:03:00 -0300

    agregue funcion para que el usuario pueda interactuar

--- app.js
+++ app.js
@@ -1,48 +1,6 @@
-// alert("Bienvenido a PromptConvert, esta app permite convertir ARS a USD.");
-
-// const tasaFija = 1300
-// let history = []
-
-// function converCurrency (pesos){
-//     return pesos/tasaFija;
-// }
-// let opcion =""
-// while (opcion!=="2"){
-//  opcion = prompt(
-//      `Elija una opcion:
-//         1. Divisa (ARS -> USD)
-//         2. Salir`
-// );
-
-// switch (opcion){
-//     case "1":
-//         const ars = prompt("Ingrese Monto en Pesos ARG")
-//         break
-//         const usd =converCurrency(ars)
-//         alert(`${ars} equivalen a ${usd.toFixed(2)}`)
-//         history.push ({
-//             tipo: "Divisa",
-//             entrada:`${ars} ARS`,
-//             salida: `${usd.toFixed(2)}`,
-//             fecha: new Date().toLocaleString()
-//         })
-//         break
-//     case "2":
-//         alert("Saliendo. Gracias por usar mi app :D")
-//         break
-//     default:
-//         alert("Opcion invalida. Intente nuevamente")
-// }
-// }
-// console.log (`Historial de Operaciones: ${history}`)
-
-
-// app.js
-
-
 const tasa = 1300;
 const historial = [];
-
+//funcion para convertir
 function convertir() {
     const ars = parseFloat(prompt("Ingrese cantidad en ARS:"));
     
@@ -54,14 +12,13 @@ function convertir() {
     const usd = ars/tasa;
     alert(`${ars} ARS = ${usd.toFixed(2)} USD`);
 
-    // Guardar en historial
+    // guardo en historial
     historial.push({
         fecha: new Date().toLocaleTimeString(),
         ars: ars,
         usd: usd.toFixed(2)
     });
 }
-
 function menu() {
     let opcion;
     
@@ -72,14 +29,9 @@ function menu() {
             2. Salir`
         );
 
-        switch (opcion) {
-            case "1": convertir(); break;
-            case "2": 
-                console.log("Historial completo:", historial); 
-                break;
-            default: alert("Opción no válida");
-        }
-    } while (opcion !== "2");
-}
 
-menu();
+        
+    }while(opcion !== "2");
+
+
+}menu();
